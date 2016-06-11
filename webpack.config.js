d8065019b54299ae2bbe153481a91bfee3883bf9
Joshua Comeau <joshwcomeau@gmail.com>
2016-06-11 08:29:57 -0400

    Set up CarteBlanche with a simple demo and a sampkle component.

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CarteBlanche = require('carte-blanche');
+
+module.exports = {
+  devtool: 'eval',
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+
+  entry: [
+    'webpack-dev-server/client',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, './demo/index.js'),
+  ],
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.join(__dirname, './demo/index.html'),
+    }),
+    new CarteBlanche({
+      componentRoot: './src',
+      dest: 'components'
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel'],
+        exclude: /node_modules/,
+        include: [
+          path.join(__dirname, './src'),
+          path.join(__dirname, './demo'),
+        ],
+      },
+    ],
+  },
+  devServer: {
+    // It suppress error shown in console, so it has to be set to false.
+    // quiet: false,
+    // It suppress everything except error, so it has to be set to false as well
+    // to see success build.
+    noInfo: true,
+    stats: {
+      // Config for minimal console.log mess.
+      assets: false,
+      colors: true,
+      version: false,
+      hash: false,
+      timings: false,
+      chunks: false,
+      chunkModules: false,
+    },
+  },
+};
